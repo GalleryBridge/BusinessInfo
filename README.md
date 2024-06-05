532003715cb117ec773f3064ca6929abef6cd109
laidoring@163.com <Laidoring@163.com>
2024-06-05 23:47:42 +0800

    clear model

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Business-Info
-Business Chart
+temp 项目模板 包含登录注册
 
 智能生成商业可视化报表
--- business-backend/sql/create_table.sql
+++ business-backend/sql/create_table.sql
@@ -22,18 +22,3 @@ create table if not exists user
     index idx_userAccount (userAccount)
 ) comment '用户' collate = utf8mb4_unicode_ci;
 
--- 帖子表
-create table if not exists post
-(
-    id         bigint auto_increment comment 'id' primary key,
-    title      varchar(512)                       null comment '标题',
-    content    text                               null comment '内容',
-    tags       varchar(1024)                      null comment '标签列表（json 数组）',
-    thumbNum   int      default 0                 not null comment '点赞数',
-    favourNum  int      default 0                 not null comment '收藏数',
-    userId     bigint                             not null comment '创建用户 id',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete   tinyint  default 0                 not null comment '是否删除',
-    index idx_userId (userId)
-) comment '帖子' collate = utf8mb4_unicode_ci;
