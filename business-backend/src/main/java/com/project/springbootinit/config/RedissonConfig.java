acb94c2c8e8c3aed97deb129f686751780a23c4d
laidoring@163.com <Laidoring@163.com>
2024-06-12 00:16:29 +0800

    add a new bug

---
+++ business-backend/src/main/java/com/project/springbootinit/config/RedissonConfig.java
@@ -0,0 +1,37 @@
+package com.project.springbootinit.config;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.redis")       //  动态读取配置 读取yml中的Redis配置
+@Data
+@Slf4j
+public class RedissonConfig {
+
+    private Integer database;
+
+    private String host;
+
+    private Integer port;
+
+    private String password;
+
+    @Bean
+    public RedissonClient redissonClient(){
+        Config config = new Config();
+        config.useSingleServer()
+                .setDatabase(database) //  这里使用 2库来实现限流
+                .setAddress("redis://" + host + ":" + port)
+                .setPassword(password);
+        return Redisson.create(config);
+    }
+
+
+}
---
+++ business-backend/src/main/java/com/project/springbootinit/manager/RedisLimitManager.java
@@ -0,0 +1,33 @@
+package com.project.springbootinit.manager;
+
+import com.project.springbootinit.common.ErrorCode;
+import com.project.springbootinit.exception.BusinessException;
+import org.redisson.api.RRateLimiter;
+import org.redisson.api.RateIntervalUnit;
+import org.redisson.api.RateType;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ *  专门提供 RedisLimit 限流基础服务的 (提供了通用的能力)
+ */
+
+@Service
+public class RedisLimitManager {
+
+    @Resource
+    private RedissonClient redissonClient;
+
+    public void doRateLimit(String key){
+        //  创建一个名称为user_limiter的限流器 每秒最多5次
+        RRateLimiter rateLimiter = redissonClient.getRateLimiter(key);
+        rateLimiter.trySetRate(RateType.OVERALL, 5, 1, RateIntervalUnit.SECONDS);
+
+        //  这里定义的是可操作的令牌数
+        boolean canOp = rateLimiter.tryAcquire(1);
+        if (!canOp)
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+    }
+}
